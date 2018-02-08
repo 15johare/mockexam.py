d70e2d1a9a0d885eae49fdd59d349554dd8dee60
15johare <15johare@stjosephsrush.ie>
2018-02-08 14:43:52 +0000

    Update mockexam.py

--- mockexam.py
+++ mockexam.py
@@ -1,3 +1,45 @@
 while True:
- print
+ 
+ mood=input("choose what mood you are feeling")
+ 
+ print("this is a program that shows you what music to listen to depending on your mood")
+ print("all you have to do is type what mood you are and a link to a song will come up")
+ 
+ if mood=="happy":
+     print("https://www.youtube.com/watch?v=ZbZSe6N_BXs")
+
+ elif mood=="sad":
+     print("https://www.youtube.com/watch?v=aWIE0PX1uXk")
+
+ elif mood=="angry":
+     print("https://www.youtube.com/watch?v=77tvDMkc9eE&list=PLclxqlStifc4RjX5KM3NlIyn3RyN3aydk")
+
+ elif mood=="scared":
+     print("https://www.youtube.com/watch?v=e2vBLd5Egnk")
+
+ elif mood=="love":
+     print("https://www.youtube.com/watch?v=2Vv-BfVoq4g")
+
+ elif mood=="confusion":
+     print("https://www.youtube.com/watch?v=g2R0V-TgBY4")
+
+ elif mood=="heart break":
+     print("https://www.youtube.com/watch?v=GSBFehvLJDc")
+ 
+ elif mood=="ecstatic":
+     print("https://www.youtube.com/watch?v=nbH7SHKoZYY")
+
+ elif mood=="hyper":
+     print("https://www.youtube.com/watch?v=XbGs_qK2PQA")
+
+ elif mood=="jealous":
+     print("https://www.youtube.com/watch?v=50VWOBi0VFs")
+
+ elif mood=="lonely":
+     print("https://www.youtube.com/watch?v=50VWOBi0VFs")
+
+ else :
+     print("that is not a mood")
+   
+
  
